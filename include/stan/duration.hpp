7f1b9d0613ace5eeef0687a35537da9b81669d4b
John Galbraith <john.galbraith17@gmail.com>
2018-11-03 19:42:38 -0700

    Refactor duration for style

--- include/stan/duration.hpp
+++ include/stan/duration.hpp
@@ -4,26 +4,22 @@
 
 namespace stan {
 
+// Durations model musical time, and represent sums of note values.
+
 struct duration : rational<std::uint32_t>
 {
     using rational<std::uint32_t>::rational;
 
-    duration operator+(duration const &d2) const
-    {
-        // https://www.geeksforgeeks.org/program-to-add-two-fractions
-        assert(d2.den() > 0);
-        integer gcd = compute_gcd(den(), d2.den());
-        integer d = (den() * d2.den()) / gcd;
-        integer n = (num() * d) / den() + (d2.num() * d) / d2.den();
-        return { n, d };
-    }
+    friend duration operator+(duration const &d1, duration const &d2);
+    friend duration operator*(int, duration const &);
+
+    // Arbitrary durations can be constructed in very few places, as a static
+    // safety feature.  Normally they are queried from objects, accumulated
+    // with operator+ or operator*, or initialized with duration::zero().
 
     static duration zero() { return duration(0, 1); }
-
     friend struct value;
     friend struct tuplet;
-
-    friend duration operator*(int, duration const &);
 };
 
 } // namespace stan
--- src/notation/duration.cpp
+++ src/notation/duration.cpp
@@ -3,6 +3,22 @@
 
 namespace stan {
 
+duration operator+(duration const &d1, duration const &d2)
+{
+    using integer = duration::integer;
+
+    integer d1_den = d1.den();
+    integer d2_den = d2.den();
+    integer gcd = duration::compute_gcd(d1_den, d2_den);
+    assert(d2_den > 0); // Silence Division by Zero check
+    assert(gcd > 0);    // Silence Division by Zero check
+
+    // https://www.geeksforgeeks.org/program-to-add-two-fractions
+    integer d = (d1_den * d2_den) / gcd;
+    integer n = (d1.num() * d) / d1_den + (d2.num() * d) / d2_den;
+    return { n, d };
+}
+
 duration operator*(int times, duration const &dur)
 {
     return { times * dur.num(), dur.den() };
