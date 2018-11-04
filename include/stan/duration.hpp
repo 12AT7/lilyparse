6a4ea585eb91e2cf5dda6c42f2dfd212b8a48a99
John Galbraith <john.galbraith17@gmail.com>
2018-11-03 18:20:16 -0700

    Attempt to kill static analysis error on CircleCI

--- include/stan/duration.hpp
+++ include/stan/duration.hpp
@@ -11,6 +11,7 @@ struct duration : rational<std::uint32_t>
     duration operator+(duration const &d2) const
     {
         // https://www.geeksforgeeks.org/program-to-add-two-fractions
+        assert(d2.den() > 0);
         integer gcd = compute_gcd(den(), d2.den());
         integer d = (den() * d2.den()) / gcd;
         integer n = (num() * d) / den() + (d2.num() * d) / d2.den();
